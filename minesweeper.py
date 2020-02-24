85ca5714fc7ed4a4170a8072bdfa2527689d9f20
Ashley Davey <ashleydavey@live.co.uk>
2020-02-24 13:39:57 +0000

    Initial commit

---
+++ minesweeper.py
@@ -0,0 +1,36 @@
+import numpy as np
+from argparse import ArgumentParser
+
+
+if __name__ == '__main__':
+    #Handle command lone arguments
+    parser = ArgumentParser(description="Minesweeper")
+    parser.add_argument("--mines", "-m", type=int, default = 8, help = 'Number of mines in grid')
+    parser.add_argument("--size", "-s", type=int, default = 8, help = 'Width and height of (square) grid')
+    arguments = parser.parse_args()
+    size = arguments.size
+    mines = arguments.mines
+    
+    #initialise grid as 1d vector, shuffle, convert to 2d
+    grid = np.array(['X'] * mines + [''] * (size ** 2 - mines))
+    np.random.shuffle(grid)
+    grid = grid.reshape(size,size)
+    
+    #find adjacent mines to set each grid space
+    for i in range(size):
+        for j in range(size):
+            if grid[i,j] != 'X':
+                counter = 0
+                for k in [-1,0,1]:
+                    for l in [-1,0,1]:
+                        if i + k > -1 and i + k < size and j + l > -1 and j + l < size:
+                            counter += grid[i+k,j+l]=='X'
+                grid[i,j] = str(counter) 
+    
+    #output grid using discord 'spoiler' markdown, and code blocks to make the grid look even
+    print('Minesweeper! X = mine, number is how many mines are adjacent to the square. Have fun!')             
+    for line in grid:
+    
+        for element in line:
+            print('||`' + element + '`||', end = ' ')
+        print('')
---
+++ readme.md
@@ -0,0 +1,11 @@
+#Python script to generate Minesweeper for discord
+
+Simply run the code, the copy-paste the result into a discord chat. You need to install numpy (`pip install numpy`) for it to run.
+
+##Usage:
+
+```
+python minesweeper.py -s S -m M
+```
+
+where `S` is the size of the grid (default 8) and `M` is the number of mines (default 8)
